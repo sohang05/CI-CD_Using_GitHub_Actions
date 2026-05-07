8c0e6a5869500b8318a8c96175a5f81eb957e0ff
Sohan Gurav <73344291+sohang05@users.noreply.github.com>
2026-05-07 21:46:10 +0530

    Test automatic rollback with broken deployment

--- app/app.py
+++ app/app.py
@@ -3,6 +3,9 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-   return "Version 2 - testing rollback 🚀"
+   return 1 / 0
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
+
+    

4038bf698b6d8586f7c74cad5931732ea20a76e5
Sohan Gurav <73344291+sohang05@users.noreply.github.com>
2026-05-07 19:48:05 +0530

    Test rollback - version 2

--- app/app.py
+++ app/app.py
@@ -3,7 +3,6 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "One last check on the CI/CD Pipeline 🚀"
-
+   return "Version 2 - testing rollback 🚀"
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
